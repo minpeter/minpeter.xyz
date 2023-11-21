1f8f8fca510d0f23cc053bac58724edade371ac5
Woonggi Min <minpeter@friendli.ai>
2023-11-21 17:13:11 +0900

    Update button text in Button component

--- components/mdx/Button.tsx
+++ components/mdx/Button.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from "react";
 
-export default function Button({ text }: { text: string }) {
+export default function Button() {
   const [toggle, setToggle] = useState(false);
 
   return (
@@ -11,7 +11,7 @@ export default function Button({ text }: { text: string }) {
       className="bg-slate-700 rounded-md px-4 py-2"
       onClick={() => setToggle(!toggle)}
     >
-      {toggle ? text : "Click Me"}
+      {toggle ? "Push Me :)" : "You pushed me!!"}
     </button>
   );
 }
