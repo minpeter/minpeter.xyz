960bed74b6196fb8240a232fb26b1bca44e0b7af
minpeter <minpeter@friendli.ai>
2024-02-21 14:40:26 +0900

    Refactor fontSans declaration in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
