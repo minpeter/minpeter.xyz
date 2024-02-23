b0f5ad5c4c8ddbcf7825a27be36ae2dc08aaf9d0
minpeter <minpeter@friendli.ai>
2024-02-23 19:39:24 +0900

    Add mobile and desktop navigation menus

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,24 @@ import { useToast } from "@/components/ui/use-toast";
 
 import Link from "next/link";
 
-import { GitHubLogoIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
+import {
+  GitHubLogoIcon,
+  InstagramLogoIcon,
+  HamburgerMenuIcon,
+} from "@radix-ui/react-icons";
 
 import { buttonVariants } from "@/components/ui/button";
 
 import { ModeToggle } from "@/components/theme-toggle";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 import AnimatedName from "./animated-name";
 
@@ -16,12 +29,70 @@ export default function Header() {
 
   return (
     <header>
-      <div className="flex flex-col sm:flex-row sm:items-center justify-between py-4">
+      <div className="flex flex-row items-center justify-between py-4">
         <Link href="/" className="flex flex-col gap-3">
           <AnimatedName name="minpeter - 민웅기" />
           <p>software engineer 🕊️</p>
         </Link>
-        <div className="flex flex-col justify-end items-end">
+
+        {/* mobile navbar */}
+
+        <div className="flex sm:hidden">
+          <Link
+            href="https://github.com/minpeter"
+            target="_blank"
+            className={buttonVariants({
+              variant: "ghost",
+              size: "icon",
+            })}
+          >
+            <GitHubLogoIcon className="h-4 w-4" />
+          </Link>
+
+          <Link
+            href="https://instagram.com/minpeter2"
+            target="_blank"
+            className={buttonVariants({
+              variant: "ghost",
+              size: "icon",
+            })}
+          >
+            <InstagramLogoIcon className="h-4 w-4" />
+          </Link>
+
+          <ModeToggle />
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="ghost" size="icon" className="sm:hidden">
+                <HamburgerMenuIcon className="h-6 w-6" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-fit mr-8 sm:hidden">
+              <div className="grid gap-4">
+                <div className="flex-col justify-end items-end flex">
+                  <div className="flex gap-4">
+                    <Link href="/work">work</Link>
+                    <Link href="/blog">blog</Link>
+                    <div
+                      onClick={() =>
+                        toast({
+                          title: "Coming soon",
+                          description: "This feature is not available yet",
+                        })
+                      }
+                    >
+                      guestbook
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+
+        {/* desktop navbar */}
+        <div className="flex-col justify-end items-end hidden sm:flex">
           <div className="flex mr-[-0.7rem]">
             <Link
               href="https://github.com/minpeter"
