e407928066fa3c9f4e9a3365804acdfb33cd3772
minpeter <minpeter@friendli.ai>
2024-03-14 17:28:56 +0900

    Update page header description

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <section className="flex flex-col gap-3">
-      <Header description="민웅기가 가장 신경 쓴 웹이려나" />
+      <Header description="이 웹에서 가장 멋진 사이트가 될거야~" />
 
       <Link href="/blog" className="underline">
         내가 만든 블로그, 너를 위해 써봤지
