4ea06cd3be3c7ef6760386d8cec7d25974ff343d
minpeter <minpeter@friendli.ai>
2024-03-14 17:47:59 +0900

    Add icons to header links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
-import { CookieIcon } from "@radix-ui/react-icons";
+import { CommitIcon, CookieIcon } from "@radix-ui/react-icons";
+import { CodeIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Page() {
@@ -10,6 +11,15 @@ export default function Page() {
       <Link href="/blog" className="underline">
         내가 만든 블로그, 너를 위해 써봤지
         <CookieIcon className="w-4 h-4 inline-block ml-1.5" />
+      </Link>
+
+      <Link
+        href="https://ip.minpeter.tech/"
+        target="_blank"
+        className="underline"
+      >
+        시간은 없어도 IP 주소는 있어
+        <CodeIcon className="w-4 h-4 inline-block ml-1.5" />
       </Link>
     </section>
   );
