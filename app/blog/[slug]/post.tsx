38c177b29d79ff547d2cff6adb355a5fe68d8980
minpeter <kail2005611@gmail.com>
2024-04-01 23:01:56 +0900

    a

--- app/blog/[slug]/post.tsx
+++ app/blog/[slug]/post.tsx
@@ -24,55 +24,14 @@ export default function PostContent({ code }: any) {
   return (
     <Component
       components={{
-        h3: (props: any) => (
-          <h3
-            {...props}
-            className="scroll-m-20 border-b pb-2 text-base font-semibold tracking-tight first:mt-0"
-          />
-        ),
-        p: (props: any) => <p {...props} className="py-3 text-gray-400" />,
-        strong: (props: any) => <strong {...props} className="text-white" />,
-        table: (props: any) => (
-          <Table {...props} className="my-4 text-gray-400" />
-        ), // "w-full" is not a valid class name
+        table: (props: any) => <Table {...props} />, // "w-full" is not a valid class name
         th: (props: any) => <TableHead {...props} />,
         td: (props: any) => <TableCell {...props} />,
         tbody: (props: any) => <TableBody {...props} />,
         thead: (props: any) => <TableHeader {...props} />,
         tr: (props: any) => <TableRow {...props} />,
-
-        // a: (props: any) => (
-        //   <a
-        //     {...props}
-        //     className="text-gray-400 break-all underline hover:underline focus:underline"
-        //   />
-        // ),
-
-        ul: (props: any) => <ul {...props} className="list-disc pl-8 " />,
-        ol: (props: any) => <ol {...props} className="list-decimal pl-8 " />,
-        li: (props: any) => <li {...props} className="text-gray-400" />,
-
-        hr: (props: any) => <hr {...props} className="my-8" />,
-        blockquote: (props: any) => (
-          <blockquote
-            {...props}
-            className={cn(
-              "scroll-m-20 border-l-4 border-gray-200 pl-4",
-              "max-w-96",
-              "[&>ul]:flex [&>ul]:justify-end",
-              "[&>ul>li]:list-none"
-            )}
-          />
-        ),
-
         img: (props: any) => (
-          <Image
-            className="rounded-lg object-cover border shadow-sm min-w-full"
-            width={500}
-            height={300}
-            src={props.src}
-            alt={props.alt}
-          />
+          <Image width={500} height={1} src={props.src} alt={props.alt} />
         ),
 
         code: ({ children }: any) => {
@@ -90,15 +49,6 @@ export default function PostContent({ code }: any) {
             />
           );
         },
-        pre: (props: any) => (
-          <pre
-            {...props}
-            className={cn(
-              "rounded-lg border bg-card text-card-foreground shadow-sm",
-              "my-4 p-4 overflow-scroll "
-            )}
-          />
-        ),
       }}
     />
   );
