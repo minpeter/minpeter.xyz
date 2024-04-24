ee3f0d936f606242fa74434ab9c35ea4ec3642c0
minpeter <minpeter@friendli.ai>
2024-04-24 10:09:09 +0900

    Update CSS styles for code blocks and lists in mdx.css

--- app/blog/[slug]/post.tsx
+++ app/blog/[slug]/post.tsx
@@ -1,55 +1,31 @@
 "use client";
 
-import { useMemo } from "react";
-
-import { codeVariants } from "@/components/ui/typography";
-import { cn } from "@/lib/utils";
 import copy from "clipboard-copy";
 import { highlight } from "sugar-high";
 import "@/styles/mdx.css";
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-
 import { getMDXComponent } from "mdx-bundler/client";
 
 export default function PostContent({ code }: any) {
-  const Component = useMemo(() => getMDXComponent(code), [code]);
+  const Component = getMDXComponent(code);
   return (
     <Component
       components={{
-        table: (props: any) => <Table {...props} />,
-        th: (props: any) => <TableHead {...props} />,
-        td: (props: any) => <TableCell {...props} />,
-        tbody: (props: any) => <TableBody {...props} />,
-        thead: (props: any) => <TableHeader {...props} />,
-        tr: (props: any) => <TableRow {...props} />,
-
         code: ({ children, className }: any) => {
           const isMultiline = children.includes("\n");
-
           const match = /language-(\w+)/.exec(className || "");
           const language = match ? match[1] : "";
-
           const isPlain =
             language === "plaintext" ||
             language === "text" ||
             language === "plain" ||
             language === "nohighlight" ||
             language === "";
-
           return (
             <code
               onClick={() => {
                 handleCopyClick(children);
               }}
-              className={isMultiline ? "" : cn(codeVariants(), "break-all")}
               dangerouslySetInnerHTML={{
                 __html:
                   isMultiline && !isPlain ? highlight(children) : children,
