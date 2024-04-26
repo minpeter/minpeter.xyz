9dc6815304a52520bdd25a482f567b49f324632a
minpeter <minpeter@friendli.ai>
2024-04-26 14:28:17 +0900

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://blog.minpeter.xyz/sitemap.xml",
+    sitemap: "https:/minpeter.xyz/sitemap.xml",
   };
 }
