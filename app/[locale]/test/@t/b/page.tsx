f95a56d0a71ea7bda36d0834654e1d17d3c8f9b8
minpeter <minpeter@friendli.ai>
2024-11-25 13:26:45 +0900

    Add initial page and layout components for locale testing

---
+++ app/[locale]/test/@t/b/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>B</h1>
+    </div>
+  );
+}
---
+++ app/[locale]/test/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({ t }: { t: React.ReactNode }) {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <hr />
+      {t}
+    </div>
+  );
+}
