5e8689d03f38bab91e25f275b825e9ab51c214f7
minpeter <minpeter@friendli.ai>
2024-11-27 11:30:20 +0900

    Refactor sentence generation prompts and configurations for Korean and English in typing action

--- app/[locale]/typing/action.ts
+++ app/[locale]/typing/action.ts
@@ -3,23 +3,41 @@
 import { friendli } from "@friendliai/ai-provider";
 import { generateText } from "ai";
 
-export async function nextSentencesGenerator(locale: "ko" | "en") {
-  const { text } = await generateText({
-    model: friendli("meta-llama-3.1-8b-instruct"),
-    temperature: 1.4,
-    frequencyPenalty: 1.1,
-    topP: 0.1,
-    maxTokens: 50,
-    system: `You are a beautiful sentence generator for typing practice.
+const koreanPrompt = `You are a beautiful sentence generator for typing practice.
 Please write a heart-touching sentence by referring to famous movies, music, literature, comics, etc.
-Please use only ${
-      locale == "ko" ? "Korean" : "English"
-    }. Please make your answer short and concise.
+Please use only Korean. Please make your answer short and concise.
 Please avoid using special characters such as '"', '(', ')' as much as possible, as they interfere with typing practice.
 Never mention the source or original of a sentence under any circumstances. It seriously reduces the quality of the sentence.
 Even if a sentence has been translated, never say anything about it being translated.
-End your sentences smoothly, always write literary.`,
-    prompt: "Please create a phrase for typing practice, just one sentence.",
+End your sentences smoothly, always write literary.`;
+
+const englishPrompt = `You are a beautiful sentence generator for typing practice
+Please avoid using special characters such as '"', '(', ')' as much as possible, as they interfere with typing practice.
+Choose more creative sentences instead of words that start with "The.."
+`;
+
+const koreanConfig = {
+  model: friendli("meta-llama-3.1-8b-instruct"),
+  temperature: 1.4,
+  frequencyPenalty: 1.1,
+  topP: 0.1,
+  maxTokens: 50,
+  system: koreanPrompt,
+  prompt: "Please create a phrase for typing practice, just one sentence.",
+};
+
+const englishConfig = {
+  model: friendli("meta-llama-3.1-8b-instruct"),
+  temperature: 1,
+  topP: 1,
+  maxTokens: 50,
+  system: englishPrompt,
+  prompt: "Please create a phrase for typing practice, just one sentence.",
+};
+
+export async function nextSentencesGenerator(locale: "ko" | "en") {
+  const { text } = await generateText({
+    ...(locale === "ko" ? koreanConfig : englishConfig),
   });
 
   const textWithoutQuotes = text.replace(/^"([^"]+)"$/, "$1");
