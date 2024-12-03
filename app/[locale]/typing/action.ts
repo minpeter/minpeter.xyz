5c24368c73f8745d0a5e21938840a2b69b385d53
minpeter <minpeter@friendli.ai>
2024-12-03 13:13:37 +0900

    Enhance typing prompts for Korean and English configurations for clarity and completeness

--- app/[locale]/typing/action.ts
+++ app/[locale]/typing/action.ts
@@ -3,33 +3,30 @@
 import { friendli } from "@friendliai/ai-provider";
 import { generateText } from "ai";
 
-const koreanPrompt = `You are a beautiful sentence generator for typing practice.
-Please write a heart-touching sentence by referring to famous movies, music, literature, comics, etc.
-Please use only Korean. Please make your answer short and concise.
-End your sentences smoothly, always write literary.`;
-
-const englishPrompt = `You are a beautiful sentence generator for typing practice.`;
-
 const koreanConfig = {
   model: friendli("meta-llama-3.1-8b-instruct", {
-    regex: "[\n ,.?!0-9\uac00-\ud7af]*",
+    regex: "[ ,.?!0-9\uac00-\ud7af]*",
   }),
   temperature: 1.4,
   frequencyPenalty: 1.1,
   topP: 0.1,
   maxTokens: 50,
-  system: koreanPrompt,
+  system: `You are a beautiful sentence generator for typing practice.
+Please write a heart-touching sentence by referring to famous movies, music, literature, comics, etc.
+Please use only Korean. Please make your answer short and concise.
+Never mention the source or original of a sentence under any circumstances.
+End your sentences smoothly, always write literary.`,
   prompt: "Please create a phrase for typing practice, just one sentence.",
 };
 
 const englishConfig = {
   model: friendli("meta-llama-3.1-8b-instruct", {
-    regex: "[\n ,.?!0-9a-zA-Z]*",
+    regex: "[ ,.?!0-9a-zA-Z]*",
   }),
   temperature: 1,
   topP: 1,
   maxTokens: 50,
-  system: englishPrompt,
+  system: "You are a beautiful sentence generator for typing practice.",
   prompt: "Please create a phrase for typing practice, just one sentence.",
 };
 
@@ -38,6 +35,5 @@ export async function nextSentencesGenerator(locale: "ko" | "en") {
     ...(locale === "ko" ? koreanConfig : englishConfig),
   });
 
-  const textWithoutQuotes = text.replace(/^"([^"]+)"$/, "$1");
-  return textWithoutQuotes;
+  return text;
 }
