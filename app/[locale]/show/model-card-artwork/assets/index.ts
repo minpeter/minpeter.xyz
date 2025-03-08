b31f9067905c090f1b614e260887601af3ef3294
minpeter <kali2005611@gmail.com>
2025-03-09 01:36:01 +0900

    Add model card artwork images and create page component for display

---
+++ app/[locale]/show/model-card-artwork/assets/index.ts
@@ -0,0 +1,5 @@
+import hermes3 from "./hermes3-model.png";
+import llama3p1 from "./llama3p1-model.png";
+import qwen2p5 from "./qwen2p5-model.png";
+
+export { hermes3, llama3p1, qwen2p5 };
---
+++ app/[locale]/show/model-card-artwork/page.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header";
+
+import { hermes3, llama3p1, qwen2p5 } from "./assets";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <section className="flex flex-col gap-3">
+      <Header
+        title="/show/model-card-artwork"
+        link={{ href: "/show", text: "showcase로 돌아가기" }}
+      />
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 items-center">
+        <Image src={llama3p1} alt="llama3.1" />
+        <Image src={hermes3} alt="hermes3" />
+        <Image src={qwen2p5} alt="qwen2.5" />
+      </div>
+    </section>
+  );
+}
